907da7c9eea919c7ed9f65db4aa2e20e7a38a245
B004A <150476234+B004A@users.noreply.github.com>
2025-03-20 17:01:17 -0400

    leetcode 01

--- LeetCodeCS/Program.cs
+++ LeetCodeCS/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// 01
+int[] TwoSum(int[] nums, int target)
+{
+    for (int k = 0; k < nums.Length; k++)
+    {
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[k] + nums[i] == target)
+            {
+                return new int[] { k, i };
+            }
+        }
+    }
+    return null;
+}
