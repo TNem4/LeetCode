83f36c8ce8f5b70846355859723cf8e766838e83
B004A <150476234+B004A@users.noreply.github.com>
2025-03-24 17:49:49 -0400

    LC_03

---
+++ LeetCodeCS/LC_03.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCS
+{
+    //Given a string s, find the length of the longest substring without duplicate characters.
+    internal class LC_03
+    {
+        int LengthOfLongestSubstring(string s)
+        {
+            int lengthOfSubstring = 0;
+            Dictionary<char, int> charIndexMap = new Dictionary<char, int>();
+            int start = 0;
+            for(int i=0; i< s.Length; i++)
+            {
+                if (charIndexMap.ContainsKey(s[i]))
+                {
+                    start = Math.Max(charIndexMap[s[i]] + 1, start);
+                }
+                charIndexMap[s[i]] = i;
+                lengthOfSubstring = Math.Max(lengthOfSubstring, i - start + 1);
+            }
+            return lengthOfSubstring;
+
+        }
+    }
+}
